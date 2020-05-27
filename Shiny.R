1271859ada924d7d9ebe10a3e0659ffc1a68313e
Josh Park <48873009+dunnohow@users.noreply.github.com>
2020-05-28 03:14:32 +0900

    Add files via upload

--- Shiny.R
+++ Shiny.R
@@ -2,6 +2,7 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(plotly)
 
 new_confirm <- read.csv("~/Dropbox/School/UCLA/stat 141sl/Final Project/shiny/data/to.visualize2.csv")
 new_confirm <- new_confirm[,-1]
@@ -12,14 +13,30 @@ jh_testing$date <- as.Date(jh_testing$date, "%Y-%m-%d")
 jh_testing <- jh_testing %>% mutate_if(is.numeric, ~replace(., is.na(.), 0))
 
 
-mobility <- read.csv("~/Dropbox/School/UCLA/Datafest 2020/state_mobility.csv")
-mobility$date <- as.Date(mobility$date, "%Y-%m-%d")
-mobility <- gather(mobility, sector, mob, work:residential, factor_key=TRUE)
+mobility <- read.csv("~/Dropbox/School/UCLA/stat 141sl/Final Project/shiny/data/state_mobility.csv")
+mobility$date <- as.Date(mobility$date, "%m/%d/%Y")
+#mobility <- gather(mobility, sector, mob, work:residential, factor_key=TRUE)
 mobility$sub_region_1 <- as.character(mobility$sub_region_1)
-mobility[which(mobility$sub_region_1 == "California"),3] = "CA"
-mobility[which(mobility$sub_region_1 == "Florida"),3] = "FL"
-mobility[which(mobility$sub_region_1 == "New York"),3] = "NY"
-mobility$sub_region_1 %>% unique()
+
+ca_m <- mobility %>% filter(sub_region_1 == "CA") #select state
+x <- ca_m$date %>% unique()
+fig <- plot_ly(ca_m,x = ~x, y = ca_m$work, name = 'Work Sector', type = 'scatter', mode = 'lines')
+fig <- fig %>% add_trace(y = ca_m$retail, name = 'Retail Sector', mode = 'lines') 
+fig <- fig %>% add_trace(y = ca_m$transit, name = 'Transit Sector', mode = 'lines') 
+fig <- fig %>% add_trace(y = ca_m$park, name = 'Park Sector', mode = 'lines') 
+fig <- fig %>% add_trace(y = ca_m$grocery, name = 'Grocery Sector', mode = 'lines') 
+fig <- fig %>% add_trace(y = ca_m$residential, name = 'Residential Sector', mode = 'lines') 
+fig <- fig %>% layout(
+  title = 'Mobility',
+  xaxis = list(
+    title = 'date'
+  ),
+  yaxis = list(
+    title = 'scaled mobility'
+  )
+)
+
+fig
 
 #mobility %>% filter(sub_region_1 == "California") %>% ggplot() + geom_line(aes(x = date, y = mob, col = sector))
 
@@ -28,7 +45,10 @@ ui <- fluidPage(
   fluidRow(
     column(3,selectInput("state", 
                 label = "Choose a state to display",
-                choices = c("CA", "FL", "NY", "OK", "SD","WA", "WY"),
+                choices = c("AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA",
+                            "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO",
+                            "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK",
+                            "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"),
                 selected = "CA")),
     
     column(3,sliderInput("DatesMerge",
@@ -37,20 +57,13 @@ ui <- fluidPage(
                 max = as.Date("2020-05-16","%Y-%m-%d"),
                 value=c(as.Date("2020-01-23"),as.Date("2020-02-23")),timeFormat="%Y-%m-%d")),
     
-    column(3,checkboxGroupInput("checkGroup", 
-                       "Choose which sector you want to see", 
-                       choices = list("work" = "work", 
-                                      "retail" = "retail", 
-                                      "transit" = "transit",
-                                      "park" = "park",
-                                      "grocery" = "grocery",
-                                      "residential" = "residential"),
-                       selected = "work"))
+    column(3,h4("Choose or drop sector by clicking legends"))
   ),
   splitLayout(
     plotOutput("ts_plot"),
-    plotOutput("mob_plot")
-))
+    plotlyOutput("mob_plot")
+  )
+)
 
 server <- function(input, output) {
   datasetInput <- reactive({
@@ -58,12 +71,7 @@ server <- function(input, output) {
   })
   
   datasetInput2 <- reactive({
-    if(!is.na(input$checkGroup[1])){
-      mobility %>% filter(sub_region_1 == input$state & sector == input$checkGroup[1] & date > input$DatesMerge[1] & date < input$DatesMerge[2])
-    }
-    else{
       mobility %>% filter(sub_region_1 == input$state & date > input$DatesMerge[1] & date < input$DatesMerge[2])
-    }
     
   })
   
@@ -76,11 +84,6 @@ server <- function(input, output) {
           input$DatesMerge[1], "to", input$DatesMerge[2])
   })
   
-  output$choice <- renderText({
-    paste("This is your choice",
-          input$checkGroup[1], "sector")
-  })
-  
   output$ts_plot <- renderPlot({
     dataset <- datasetInput()
     ggplot(dataset, aes(x = date)) + geom_line(aes(y = count, col = for.what)) +
@@ -88,10 +91,26 @@ server <- function(input, output) {
       labs(colour="Legend") +
       ggtitle("Count for Confirmed, new positive, deaths")})
   
-  output$mob_plot <- renderPlot({
+  output$mob_plot <- renderPlotly({
     dataset <- datasetInput2()
-    dataset %>% ggplot() + geom_line(aes(x = date, y = mob)) +
-      ggtitle("Mobility of the Sector that you choose")})
-}
+    x <- dataset$date %>% unique()
+    fig <- plot_ly(dataset,x = ~x, y = dataset$work, name = 'Work Sector', type = 'scatter', mode = 'lines')
+    fig <- fig %>% add_trace(y = dataset$retail, name = 'Retail Sector', mode = 'lines') 
+    fig <- fig %>% add_trace(y = dataset$transit, name = 'Transit Sector', mode = 'lines') 
+    fig <- fig %>% add_trace(y = dataset$park, name = 'Park Sector', mode = 'lines') 
+    fig <- fig %>% add_trace(y = dataset$grocery, name = 'Grocery Sector', mode = 'lines') 
+    fig <- fig %>% add_trace(y = dataset$residential, name = 'Residential Sector', mode = 'lines') 
+    fig <- fig %>% layout(
+      title = 'Mobility',
+      xaxis = list(
+        title = 'date'
+      ),
+      yaxis = list(
+        title = 'scaled mobility'
+      )
+    )
+    
+    fig
+})}
 
 shinyApp(ui = ui, server = server)
